c2220aa5e3394fbc7ee2c3ff4fc9e07438705b03
MAO-scripts <133263022+MAO-scripts@users.noreply.github.com>
2023-05-25 21:34:40 +0330

    Create config_sv.lua

---
+++ server/config_sv.lua
@@ -0,0 +1,8 @@
+Admins = {-- Please place the Steam HEX admins here to grant them access to the "addParkingMeter" command.
+    ["steam:11000013d26ca4a"] = true 
+}
+
+command = "addp" -- The command name for adding a parking meter
+
+---- LOCATIONS ----
+locations = {} --Don't change!
